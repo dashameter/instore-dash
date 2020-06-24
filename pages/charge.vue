62b04508048280a158b2a94e9eaa6e0c1f6849bd
Dashameter <dashameter@gmail.com>
2020-06-24 08:44:06 +0700

    init

---
+++ pages/charge.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Customer name:</p>
+    <v-overflow-btn :items="['USD']" label="Currency"></v-overflow-btn>
+    <v-text-field label="Order Description"></v-text-field>
+    <v-text-field label="Order Amount" type="number"></v-text-field>
+    <v-btn color="success">Confirm</v-btn>
+    <v-btn color="error">Cancel</v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style scoped></style>
